bc61a9aab810a18d2707574c3daf18ba28644012
praise159praise <praisebamiyo159@gmail.com>
2022-07-15 11:39:26 +0100

    implemented self avoiding walk

--- script.js
+++ script.js
@@ -1,67 +1,77 @@
-var x
-var y
 
-const make2dArray = (width, height, fillVal) => Array(height).fill().map(item => Array(width).fill(fillVal))
+//TO-DO : come up with better algorithm
+const make2dArray = (width, height, fillVal = null) => Array(height).fill().map(item => Array(width).fill(fillVal))
 
 let grid
 let spacing
 let cols, rows
+let path = []
+let spot
 
-let allOptions = [ /*right*/{ dx: 1, dy: 0 }, /*left*/{ dx: -1, dy: 0 }, /*down*/{ dx: 0, dy: 1 }, /*up*/{ dx: 0, dy: -1 }]
+
+
+
 function setup() {
-    createCanvas(500, 500)
-    spacing = 10
+    createCanvas(400, 400)
+    spacing = 50
     cols = floor(width / spacing)
     rows = floor(height / spacing)
-    x = cols / 2
-    y = rows / 2
+
     background(1)
-    grid = make2dArray(cols, rows, false)
-    grid[x][y] = true
+    grid = make2dArray(cols, rows)
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = new Spot(i, j)
+        }
+    }
+    spot = grid[0][0]
+    translate(spacing / 2, spacing / 2)
+    path.push(spot)
+    spot.visited = true
+
 }
 
 const isValid = (i, j) => {
     if (i < 0 || i >= cols || j < 0 || j >= rows) return false
-    return !grid[i][j]
+    return !grid[i][j].visited
 }
 function draw() {
+    background(0)
+    translate(spacing / 2, spacing / 2)
+
+
+    for (let i = 0; i <= 1; i++) {
+        spot = spot.nextSpot()
+        if (!spot) {
+            let stuck = path.pop()
+            spot = path[path.length - 1]
+            stuck.clear()
+            console.log('stuck')
+            // noLoop()
+        } else {
+            path.push(spot)
+            spot.visited = true
+        }
+
+        if (path.length == cols * rows) {
+            console.log('solved')
+            noLoop()
+        }
+    }
+    stroke(255)
+    strokeWeight(1)
+    noFill()
+    beginShape()
+
+    for (let spot of path) {
+        strokeWeight(spacing / 2)
+        point(spot.x, spot.y)
+        strokeWeight(1)
+        vertex(spot.x, spot.y)
+    }
+    endShape()
     stroke(255)
     strokeWeight(spacing / 2)
-    point(x * spacing, y * spacing)
+    point(spot.x, spot.y)
 
-    var r = floor(random(4))
-
-    let options = []
-    for (let option of allOptions) {
-        let newX = x + option.dx
-        let newY = y + option.dy
-        if (isValid(newX, newY)) options.push(option)
-    }
-
-    if (options.length > 0) {
-        let step = random(options)
-
-        stroke(255)
-        beginShape()
-        strokeWeight(1)
-        vertex(x * spacing, y * spacing)
-        x += step.dx
-        y += step.dy
-        vertex(x * spacing, y * spacing)
-        endShape()
-
-
-
-        grid[x][y] = true
-    } else {
-        console.log(" I'm stuck ")
-        noLoop()
-    }
-
-    // if (x + 10 > width || x + 10 < 0 || y + 10 > height || y + 10 < 0) {
-    //     // x=floor(random(width))
-    //     // y=floor(random(height))
-    //     x = 250
-    //     y = 250
-    // }
 }
